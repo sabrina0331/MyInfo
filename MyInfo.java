d12b0c97f69ce623113d01838cf5af5d8d432395
sabrina0331 <sabrina20520@gmail.com>
2018-10-27 23:54:36 -0700

    upload

---
+++ MyInfo.java
@@ -0,0 +1,8 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "xinya";
+        int age= 30;
+        String hometown = "Beijing,China";
+        System.out.println("My name is "+name+"\n"+"I am "+age+" years old"+"\n"+"My hometown is "+hometown);
+    }   
+}
